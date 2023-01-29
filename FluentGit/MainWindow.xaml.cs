918aac794222740d83ea0403ef7d5fce8ce1d2ab
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-01-29 16:25:30 +0700

    Make MainWindow have MinWidth and MinHeight.
     - Add PInvoke.User32 Nuget package for Windows API.
     - Add fields, methods, and structs for MainWindow to initialize MinWidth and MinHeight.

--- FluentGit/MainWindow.xaml.cs
+++ FluentGit/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Windows.Input;
 using Windows.Foundation;
@@ -33,6 +34,8 @@ namespace FluentGit
         public MainWindow()
         {
             this.InitializeComponent();
+            ApplicationReferences.MainWindowReference = this;
+            SubClassing();
         }
 
         /// <summary>
@@ -45,16 +48,68 @@ namespace FluentGit
         }
 
         /// <summary>
-        /// Event handler for MainFrame, invoke when loaded. Also is the event handler for the MainWindow,
-        /// invoke when loaded. Configure the global application's data context ApplicationDataContext to
-        /// store a reference of this MainWindow.
+        /// Event handler for MainFrame, invoke when loaded.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void MainFrame_Loaded(object sender, RoutedEventArgs e)
         {
             NavigatePage(typeof(HomePage));
-            ApplicationReferences.MainWindowReference = this;
+        }
+
+        private delegate IntPtr WinProc(IntPtr hWnd, PInvoke.User32.WindowMessage Msg, IntPtr wParam, IntPtr lParam);
+        private WinProc newWndProc = null;
+        private IntPtr oldWndProc = IntPtr.Zero;
+        [DllImport("user32")]
+        private static extern IntPtr SetWindowLong(IntPtr hWnd, PInvoke.User32.WindowLongIndexFlags nIndex, WinProc newProc);
+        [DllImport("user32.dll")]
+        static extern IntPtr CallWindowProc(IntPtr lpPrevWndFunc, IntPtr hWnd, PInvoke.User32.WindowMessage Msg, IntPtr wParam, IntPtr lParam);
+
+        private void SubClassing()
+        {
+            newWndProc = new WinProc(NewWindowProc);
+            //Get the Window's HWND
+            IntPtr hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            oldWndProc = SetWindowLong(hwnd, PInvoke.User32.WindowLongIndexFlags.GWL_WNDPROC, newWndProc);
+        }
+
+        int MinWidth = 500;
+        int MinHeight = 500;
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct MINMAXINFO
+        {
+            public PInvoke.POINT ptReserved;
+            public PInvoke.POINT ptMaxSize;
+            public PInvoke.POINT ptMaxPosition;
+            public PInvoke.POINT ptMinTrackSize;
+            public PInvoke.POINT ptMaxTrackSize;
+        }
+
+        private IntPtr NewWindowProc(IntPtr hWnd, PInvoke.User32.WindowMessage Msg, IntPtr wParam, IntPtr lParam)
+        {
+            switch (Msg)
+            {
+                case PInvoke.User32.WindowMessage.WM_GETMINMAXINFO:
+                    var dpi = PInvoke.User32.GetDpiForWindow(hWnd);
+                    float scalingFactor = (float)dpi / 96;
+
+                    MINMAXINFO minMaxInfo = Marshal.PtrToStructure<MINMAXINFO>(lParam);
+                    minMaxInfo.ptMinTrackSize.x = (int)(MinWidth * scalingFactor);
+                    minMaxInfo.ptMinTrackSize.y = (int)(MinHeight * scalingFactor);
+                    Marshal.StructureToPtr(minMaxInfo, lParam, true);
+                    break;
+
+            }
+            return CallWindowProc(oldWndProc, hWnd, Msg, wParam, lParam);
+        }
+
+        [ComImport]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        [Guid("EECDBF0E-BAE9-4CB6-A68E-9598E1CB57BB")]
+        internal interface IWindowNative
+        {
+            IntPtr WindowHandle { get; }
         }
     }
 
