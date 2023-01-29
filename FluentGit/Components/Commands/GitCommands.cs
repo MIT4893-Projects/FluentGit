8d25d0ae1f4742908e048877b57215bf6c289bf6
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-01-29 20:31:39 +0700

    Make CreateButton workable
     - Add GitCommands static class to start a git command.

---
+++ FluentGit/Components/Commands/GitCommands.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentGit.Components.Commands
+{
+    public static class GitCommands
+    {
+        private static void ConfigureProcess(Process process, string arguments)
+        {
+            process.StartInfo.FileName = "git";
+            process.StartInfo.Arguments = arguments;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+        }
+
+        private static int? TryStartProcess(Process process)
+        {
+            try
+            {
+                process.Start();
+                process.WaitForExit();
+            }
+            catch (System.ComponentModel.Win32Exception)
+            {
+                return null;
+            }
+            return process.ExitCode;
+        }
+
+        private static int? StartGitProcess(string arguments)
+        {
+            Process process = new();
+            ConfigureProcess(process, arguments);
+            return TryStartProcess(process);
+        }
+
+        public static int? Init(string dir)
+        {
+            return StartGitProcess($"init {dir}");
+        }
+    }
+}
