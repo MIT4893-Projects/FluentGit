8419ac0f4f7b0f930b923f77547ecda8fd8e9c5d
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-01-29 14:10:40 +0700

    Add "PageDataContext" for data contexts
     - This class inherited from "INotifyPropertyChanged" to supports property changed notify.

---
+++ FluentGit/PageDataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentGit
+{
+    /// <summary>
+    /// Page's Data context to use in controls.
+    /// </summary>
+    public class PageDataContext : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChange(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new(propertyName));
+        }
+    }
+}
