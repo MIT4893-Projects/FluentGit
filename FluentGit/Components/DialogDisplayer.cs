d421e66afa676e45444439fff0f6f47bffbd991d
MIT4893-Projects <minhngoc2891211@gmail.com>
2023-01-29 20:36:39 +0700

    Add DialogDisplayer static class
     - This class display dialogs, such as FolderPicker or MessageDialog.

---
+++ FluentGit/Components/DialogDisplayer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using Windows.UI.Popups;
+using Windows.Gaming.Input.ForceFeedback;
+
+namespace FluentGit.Components
+{
+    public struct DialogDisplayer
+    {
+        private static void WindowHandle(object dialog)
+        {
+            // Get the current window's HWND by passing in the Window object
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(ApplicationReferences.MainWindowReference);
+
+            // Associate the HWND with the file picker
+            WinRT.Interop.InitializeWithWindow.Initialize(dialog, hwnd);
+        }
+
+        public static async void ShowMessage(string message, string title = "")
+        {
+            MessageDialog messageDialog = new(message);
+            messageDialog.Title = title;
+            WindowHandle(messageDialog);
+            await messageDialog.ShowAsync();
+        }
+
+        public static async Task<string> ShowFolderPicker()
+        {
+            FolderPicker directoryBrowser = new();
+
+            WindowHandle(directoryBrowser);
+
+            directoryBrowser.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop;
+            directoryBrowser.FileTypeFilter.Add("*");
+
+            StorageFolder folder = await directoryBrowser.PickSingleFolderAsync();
+
+            return folder?.Path;
+        }
+    }
+}
